3e35e80f6485f11ef27f038ee1cc21a0d14c1227
HugoRoca <hugo.rock20@hotmail.com>
2019-09-17 14:41:55 -0500

    pool connections mongo and elastic

--- config.js
+++ config.js
@@ -1,6 +1,127 @@
 const config = {
-    mongo: {
-        host: ''
+    mongodb: {
+        PE: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpPeru_GANAMAS',
+        },
+        CL: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpChile_GANAMAS',
+        },
+        GT: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpGuatemala',
+        },
+        PA: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpPanama',
+        },
+        CO: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpColombia_GANAMAS',
+        },
+        BO: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpBolivia',
+        },
+        SV: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpSalvador',
+        },
+        PR: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpPuertoRico',
+        },
+        MX: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpMexico_GANAMAS',
+        },
+        EC: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpEcuador_GANAMAS',
+        },
+        DO: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpDominicana',
+        },
+        CR: {
+            connectionString: 'mongodb+srv://usrmongotdapp:Mongo2018@personalizacionqas-xfhrx.mongodb.net',
+            database: 'BelcorpCostaRica_GANAMAS',
+        },
+    },
+    elasticsearch: {
+        BO: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        CL: {
+            host: "https://vpc-es-sbsearch-ppr-rnioiss6o347c74q4w2u7w2uhu.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        CO: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        CR: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        EC: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        SV: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        GT: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        MX: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        PA: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        PE: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        PR: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        },
+        DO: {
+            host: "https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com/",
+            log: "",
+            index: "producto_v2",
+            type: "_doc"
+        }
     }
 }
 
---
+++ poolConnectionES.js
@@ -0,0 +1,61 @@
+const elastic = require("elasticsearch");
+const config = require("./config");
+
+class PoolConnectionES {
+    constructor() {
+        if (!PoolConnectionES.instance) {
+            this.connection = {};
+            PoolConnectionES.instance = this;
+        }
+
+        return PoolConnectionES.instance;
+    }
+
+    getConnection() {
+        return this.connection;
+    }
+
+    getClient(codigoPais) {
+        return this.connection[codigoPais];
+    }
+
+    getConnectionAsync(codigoPais) {
+        return new Promise((resolve, reject) => {
+            try {
+                if (this.connection[codigoPais]) {
+                    resolve(this.connection[codigoPais]);
+                } else {
+                    let host = config.elasticsearch[codigoPais].host;
+                    let log = config.elasticsearch[codigoPais].log;
+                    this.connection[codigoPais] = elastic.Client({
+                        host: `${host}`,
+                        log: `${log}`
+                    });
+                    console.log("successful connection to elasticsearch =>", codigoPais);
+                    resolve(this.connection[codigoPais]);
+                }
+            } catch (error) {
+                console.log(error);
+                reject(error);
+            }
+        });
+    }
+
+    createConnection() {
+        let promise = [];
+        let paisesHabilitados = config.constantes.paisesHabilitados;
+
+        for (const key in config.elasticsearch) {
+            if (config.elasticsearch.hasOwnProperty(key)) {
+                if (paisesHabilitados.indexOf(key) >= 0) promise.push(this.getConnectionAsync(key));
+            }
+        }
+
+        return Promise.all(promise);
+    }
+}
+
+const instance = new PoolConnectionES();
+Object.freeze(instance);
+
+module.exports = instance;
